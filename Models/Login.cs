3d8fb142612faaddd2d288c6283c7494eacb362d
OnurARIK <onrark@gmail.com>
2021-03-17 20:37:57 +0300

    Add project files.

---
+++ Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CJProje.Models
+{
+    public class Login
+    {
+        public int Id { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string userRole { get; set; }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CJProje.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string orderName { get; set; }
+        public string orderDetail { get; set; }
+    }
+}
